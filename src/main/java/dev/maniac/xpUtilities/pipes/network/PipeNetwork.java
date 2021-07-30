c9065e6932073c71e7c8d562bd1386a3e1956393
Jacob Buchanan <jbuchanan21@qub.ac.uk>
2021-07-30 18:05:13 +0100

    Add Network Manager

--- src/main/java/dev/maniac/xpUtilities/pipes/network/PipeNetwork.java
+++ src/main/java/dev/maniac/xpUtilities/pipes/network/PipeNetwork.java
@@ -1,4 +1,18 @@
 package dev.maniac.xpUtilities.pipes.network;
 
-public class PipeNetwork {
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.world.PersistentState;
+
+import java.util.Map;
+
+public class PipeNetwork extends PersistentState {
+    public static final String NAME = "xp_utilities_pipe_networks";
+
+    @Override
+    public NbtCompound writeNbt(NbtCompound nbt) {
+        for (Map.Entry<PipeNetworkType, PipeNetworkManager> entry : managers.entrySet()) {
+            nbt.put(entry.getKey().getIdentifier().toString(), entry.getValue().toTag(new NbtCompound()));
+        }
+        return nbt;
+    }
 }
